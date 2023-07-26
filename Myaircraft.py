53bb6e8b7765e6e35432978828b98deccabe270c
Zcaic <462845148@qq.com>
2023-07-27 05:35:36 +0800

    complete a basic wing

--- Myaircraft.py
+++ Myaircraft.py
@@ -1,19 +1,50 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
 from aerosandbox.geometry.airfoil.airfoil import get_UIUC_coordinates
+
+import logging
+
 from utils.dotdict import DotDict
 
-airfoils_list = DotDict(
-    rootaf=asb.Airfoil(name="rootaf", coordinates=get_UIUC_coordinates(name="rae2822")),
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-print(airfoils_list.keys())
+airfoils_list = DotDict(
+    asb.Airfoil(name="rootaf", coordinates=get_UIUC_coordinates("rae2822")),
+    asb.Airfoil(name="tipaf", coordinates=get_UIUC_coordinates("dae11")),
+)
 
-wings_list = [
+# print(airfoils_list)
+
+wings_list = DotDict(
     asb.Wing(
         name="mainWing",
-        xsecs=asb.WingXSec(
-            xyz_le=[0, 0, 0], chord=5.5, twist=0, airfoil=airfoils_list.rootaf
-        ),
+        xsecs=[
+            asb.WingXSec(
+                xyz_le=[0, 0, 0], chord=5.5, twist=0, airfoil=airfoils_list.rootaf
+            ),
+            asb.WingXSec(
+                xyz_le=[2.5, 5, 0], chord=2.5, twist=0, airfoil=airfoils_list.rootaf
+            ),
+        ],
     ),
-]
+    asb.Wing(
+        name="secondaryWing",
+        xsecs=[
+            asb.WingXSec(
+                xyz_le=[0, 0, 0], chord=1, twist=0, airfoil=airfoils_list.tipaf
+            ),
+        ],
+    ),
+)
+
+# print(airfoils_list.rootaf)
+bwb = asb.Airplane(
+    name="bwb",
+    xyz_ref=[0, 0, 0],
+    wings=[
+       wings_list.mainWing,
+    ],
+)
+bwb.draw()
--- utils/dotdict.py
+++ utils/dotdict.py
@@ -1,25 +1,25 @@
-
-
+# from typing import List
+# from aerosandbox.common import AeroSandboxObject
 
 class DotDict(dict):
     """
     Makes a  dictionary behave like an object,with attribute-style access.
     """
     def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+        # super().__init__(*args,**kwargs)
+        for i in args:
+            super().__setattr__(i.name,i)
+
 
     def __getattr__(self, key: str):
         if key in self.keys():
-            value=self[key]
-            if isinstance(value,dict):
-                value=DotDict(value)
-            return value
+            return self[key]
         else:
             raise AttributeError(f"no attribute '{key}'")
     
     def __setattr__(self, key: str, value) -> None:
-        if key in self.keys:
-            self[key]=value
+        if hasattr(self,key):
+            super().__setattr__(key,value)
         else:
             raise AttributeError('The class does not allow dynamic addition of properties')
 
