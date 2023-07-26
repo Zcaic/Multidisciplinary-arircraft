0bb495180cddf39f235005d6198a819a5b432ab3
Zcaic <462845148@qq.com>
2023-07-27 06:34:57 +0800

    delete DotDict

--- Myaircraft.py
+++ Myaircraft.py
@@ -3,48 +3,39 @@ import aerosandbox.numpy as np
 from aerosandbox.geometry.airfoil.airfoil import get_UIUC_coordinates
 
 import logging
-
-from utils.dotdict import DotDict
+from typing import Dict
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-airfoils_list = DotDict(
-    asb.Airfoil(name="rootaf", coordinates=get_UIUC_coordinates("rae2822")),
-    asb.Airfoil(name="tipaf", coordinates=get_UIUC_coordinates("dae11")),
+airfoils_list:Dict[str,asb.Airfoil]= dict(
+    rootaf=asb.Airfoil(name="rootaf", coordinates=get_UIUC_coordinates("rae2822")),
+    tipaf=asb.Airfoil(name="tipaf", coordinates=get_UIUC_coordinates("dae11")),
 )
 
 # print(airfoils_list)
 
-wings_list = DotDict(
-    asb.Wing(
+wings_list:Dict[str,asb.Wing] = dict(
+    mainWing=asb.Wing(
         name="mainWing",
         xsecs=[
             asb.WingXSec(
-                xyz_le=[0, 0, 0], chord=5.5, twist=0, airfoil=airfoils_list.rootaf
+                xyz_le=[0, 0, 0], chord=5.5, twist=0, airfoil=airfoils_list["rootaf"]
             ),
             asb.WingXSec(
-                xyz_le=[2.5, 5, 0], chord=2.5, twist=0, airfoil=airfoils_list.rootaf
+                xyz_le=[2.5, 5, 0], chord=2.5, twist=0, airfoil=airfoils_list["rootaf"]
             ),
         ],
     ),
-    asb.Wing(
+    secondaryWing=asb.Wing(
         name="secondaryWing",
         xsecs=[
             asb.WingXSec(
-                xyz_le=[0, 0, 0], chord=1, twist=0, airfoil=airfoils_list.tipaf
+                xyz_le=[0, 0, 0], chord=1, twist=0, airfoil=airfoils_list["tipaf"]
             ),
         ],
     ),
 )
 
-# print(airfoils_list.rootaf)
-bwb = asb.Airplane(
-    name="bwb",
-    xyz_ref=[0, 0, 0],
-    wings=[
-       wings_list.mainWing,
-    ],
-)
-bwb.draw()
+wings_list['mainWing'].draw()
